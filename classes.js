0f51d8c5c25ffbf7f35fcde7d7adfef77348130d
ChriswFoster <chriswfoster@gmail.com>
2018-02-20 21:13:31 -0600

    classes complete

--- classes.js
+++ classes.js
@@ -61,7 +61,21 @@ class Employee {
   Call your new class Manager
 */
 
-Manager
+class Manager {
+  constructor(first_name, last_name, email, age) {
+    ;(this.first_name = first_name),
+      (this.last_name = last_name),
+      (this.email = email),
+      (this.age = age),
+      (this.reports = []),
+      (this.hire = function(employee) {
+        this.reports.push(employee)
+      }),
+      (this.fire = function(index) {
+        this.reports.splice(index, 1)
+      })
+  }
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -85,7 +99,46 @@ Manager
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager {
+  constructor(first_name, last_name, email, age) {
+    ;(this.first_name = first_name),
+      (this.last_name = last_name),
+      (this.email = email),
+      (this.age = age),
+      (this.reports = [])
+    ;(this.title = "Not a manager"),
+      (this.bonus = 0),
+      (this.hire = function(employee) {
+        this.reports.push(employee)
+        if (this.reports.length === 0) {
+          this.title = "Not a manager"
+        } else if ((this.reports.length >= 1) & (this.reports.length <= 3)) {
+          this.title = "Barely Manager"
+        } else if ((this.reports.length >= 4) & (this.reports.length <= 10)) {
+          this.title = "Mostly Manager"
+        } else if ((this.reports.length >= 11) & (this.reports.length <= 50)) {
+          this.title = "Manager"
+        } else if ((this.reports.length >= 51) & (this.reports.length <= 100)) {
+          this.title = "Manager Plus"
+        } else this.title = "Bestest Manager"
+      }),
+      (this.fire = function(index) {
+        this.reports.splice(index, 1)
+        this.bonus += 100
+        if (this.reports.length === 0) {
+          this.title = "Not a manager"
+        } else if ((this.reports.length >= 1) & (this.reports.length <= 3)) {
+          this.title = "Barely Manager"
+        } else if ((this.reports.length >= 4) & (this.reports.length <= 10)) {
+          this.title = "Mostly Manager"
+        } else if ((this.reports.length >= 11) & (this.reports.length <= 50)) {
+          this.title = "Manager"
+        } else if ((this.reports.length >= 51) & (this.reports.length <= 100)) {
+          this.title = "Manager Plus"
+        } else this.title = "Bestest Manager"
+      })
+  }
+}
 
 ////////// PROBLEM 4 - Black Diamond //////////
 
@@ -110,4 +163,26 @@ Manager
         - It should set decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
-//Code Here
+class Machine {
+  constructor() {
+    ;(this.widgets_made_count = 0),
+      (this.wear_and_tear_count = 0),
+      (this.needs_reboot = false)
+    ;(this.makeWidgets = function(numb) {
+      this.widgets_made_count += numb
+      this.wear_and_tear_count = this.widgets_made_count / 50
+    }),
+      (this.fixMachine = function() {
+        this.needs_reboot = true
+      }),
+      (this.reboot = function() {
+        this.wear_and_tear_count -= 10
+        this.needs_reboot = false
+      })
+  }
+}
+
+let rebootComplete = function () {
+   machine.reboot();
+}
+rebootComplete()
