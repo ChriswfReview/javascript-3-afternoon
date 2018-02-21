43c0bc8f42794a6fa38b5911787bed569a056d45
ChriswFoster <chriswfoster@gmail.com>
2018-02-20 22:07:42 -0600

    Destructing complete

--- destructuring.js
+++ destructuring.js
@@ -21,8 +21,7 @@ var carDetails = {
   Use object destructuring to save the property values from the object carDetails into new variables. 
 */
 
-//Code Here
-
+var {color, make, model, year} = carDetails;
 
 
 ////////// PROBLEM 2 //////////
@@ -34,7 +33,7 @@ var carDetails = {
 */
 
 function greeting( obj ) {
-  //Code Here
+ var {firstName, lastName, title} = obj
   
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
@@ -53,8 +52,10 @@ function greeting( obj ) {
   Sum up the values and return the total number.
 */
 
-//Code Here
-
+function totalPopulation (obj) {
+  var {utah, california, texas, arizona} = obj
+  return utah + california + texas + arizona
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -67,8 +68,12 @@ function greeting( obj ) {
   Push these new variables to an array and return the array. 
 */
 
-//Code Here
-
+function ingredients (obj){
+  tempArr = []
+  var {carb, fat, protein} = obj
+  tempArr.push (carb, fat, protein)
+  return tempArr
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -85,8 +90,17 @@ function greeting( obj ) {
   Find the smallest number of the three and return that number.
 */
 
-//Code Here
+function largeNumbers(obj){
+if (obj.first > obj.second)
+{
+  return obj.second
+} else if (obj.first > obj.third){
+  return obj.third
+}else if (obj.third > obj.first){
+  return obj.first
+}
 
+}
 
 
 ////////// PROBLEM 6 //////////
@@ -97,6 +111,15 @@ function greeting( obj ) {
   Find the longest array and return that array.
 */
 
-//Code Here
-
+function numberGroups(obj){
+  if (obj.a.length < obj.b.length)
+  {
+    return obj.b
+  } else if (obj.a.length < obj.c.length){
+    return obj.c
+  }else if (obj.c.length < obj.a.length){
+    return obj.a
+  }
+  
+}
 
